c8c5f95bf6fe1c649cbc7e092694633454dbba2a
Marc Girbau <marc.girbau94@gmail.com>
2023-01-25 23:53:23 +0100

    Fix indent space in PageComponent

--- src/ts/components/PageComponent.ts/PageComponent.ts
+++ src/ts/components/PageComponent.ts/PageComponent.ts
@@ -10,10 +10,11 @@ export class PageComponent extends Component {
     this.element.innerHTML = `
     <header class="main-header">
       <h1 class="main-title">My Series</h1>
-    </header> <main class="main-content"> 
+    </header> 
+    <main class="main-content"> 
      <h2 class="main-content__title">Series list</h2>
      <section class="list">
      <section class="list">
-     </main>`;
+    </main>`;
   }
 }
