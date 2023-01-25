7e3f889f6ae2e4e3a4f4cee0311f0d8df978cac4
Marc Girbau <marc.girbau94@gmail.com>
2023-01-25 23:50:58 +0100

    Add sections to PageComponent

---
+++ src/ts/components/PageComponent.ts/PageComponent.ts
@@ -0,0 +1,19 @@
+import { Component } from "../Component/Component";
+
+export class PageComponent extends Component {
+  constructor() {
+    super("div", document.body, "container");
+  }
+
+  render() {
+    super.render();
+    this.element.innerHTML = `
+    <header class="main-header">
+      <h1 class="main-title">My Series</h1>
+    </header> <main class="main-content"> 
+     <h2 class="main-content__title">Series list</h2>
+     <section class="list">
+     <section class="list">
+     </main>`;
+  }
+}
