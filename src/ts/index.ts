62b9cc3047881330de6c25321ba0bfaf821f352d
jaumecuadrada <jaumecuadrada.audio@gmail.com>
2023-01-25 21:42:15 +0100

    Create component PageComponent

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,7 @@
 import { Component } from "./components/Component/Component";
+import { PageComponent } from "./components/Component/PageComponent.ts/PageComponent";
 import { Series } from "./types/types";
 import { SeriesStructure } from "./types/types";
 
-const appContainer = document.querySelector(".container")!;
+const appContainer = new PageComponent();
+appContainer.render();
