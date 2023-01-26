569c50dbbd1c2e149b5fe437cef20ac6de7df555
Marc Girbau <marc.girbau94@gmail.com>
2023-01-27 00:53:13 +0100

    Add rating to watched films

--- src/ts/components/StarRatingComponent/StarRatingComponent.ts
+++ src/ts/components/StarRatingComponent/StarRatingComponent.ts
@@ -1,26 +1,30 @@
 import { Component } from "../Component/Component";
 
 export class StarRatingComponent extends Component {
-  constructor(parentElement: Element) {
+  buttonsClass: string;
+
+  constructor(parentElement: Element, buttonsClass: string) {
     super("ul", parentElement, "score");
+
+    this.buttonsClass = buttonsClass;
   }
 
   render() {
     super.render();
     this.element.innerHTML = `<li class="score__star">
-                <button><i class="icon icon--score far fa-star" title="1/5"></i></button>
+                <button><i class="${this.buttonsClass}" title="1/5"></i></button>
               </li>
               <li class="score__star">
-                <button><i class="icon icon--score far fa-star" title="2/5"></i></button>
+                <button><i class="${this.buttonsClass}" title="2/5"></i></button>
               </li>
               <li class="score__star">
-                <button><i class="icon icon--score far fa-star" title="3/5"></i></button>
+                <button><i class="${this.buttonsClass}" title="3/5"></i></button>
               </li>
               <li class="score__star">
-                <button><i class="icon icon--score far fa-star" title="4/5"></i></button>
+                <button><i class="${this.buttonsClass}" title="4/5"></i></button>
               </li>
               <li class="score__star">
-                <button><i class="icon icon--score far fa-star" title="5/5"></i></button>
+                <button><i class="${this.buttonsClass}" title="5/5"></i></button>
               </li>`;
   }
 }
